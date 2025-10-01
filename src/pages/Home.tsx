605b5bd00653735a2ae239e285419da3256dc6b9
MacKenzie779 <MacKenzie779@proton.me>
2025-10-01 21:03:52 +0200

    responsiveness test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -130,6 +130,7 @@ export default function Home() {
             <TransactionAddRow accounts={accounts} onAdd={handleAddTx} />
           </div>
         </div>
+        <div className="h-12 md:h-30" aria-hidden />
       </div>
 
 {/* Right: accounts overview + Manage button */}
